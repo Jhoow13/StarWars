5cdcda81b98c6582f467146ef5916ae0b43ec992
Joel Silva <joelcsilva@outlook.com>
2017-08-02 16:42:23 -0300

    Getting person details from planet list

--- app/controllers/detailsController.js
+++ app/controllers/detailsController.js
@@ -8,8 +8,8 @@
                 $location.path('/');
             };
 
-            var personUrl = personDetailService.getProducts('url');
-            $scope.residents = [];
+            var personUrl = personDetailService.getUrl('url');
+
 
             $scope.getPersonDetails = function(){
             	swHttpService.getPerson(personUrl).then(function(response){
@@ -18,19 +18,28 @@
 	            });
             }
 
+            $scope.getPersonDetailsFromPlanetList = function(personUrlFromPlanetList){
+            	personUrl = personDetailService.addUrl(personUrlFromPlanetList);
+            	swHttpService.getPerson(personUrlFromPlanetList).then(function(response){
+            		getPlanet(response.data.homeworld);
+            		$scope.personDetails = response.data;
+	            });
+            }
+
             var getPlanet = function(planetUrl){
             	swHttpService.getPlanet(planetUrl).then(function(response){
             		$scope.personPlanet = response.data.name;
+            		$scope.residents = [];
             		var residents = response.data.residents;
 
             		residents.forEach(function(residentsUrl){
             			swHttpService.getPerson(residentsUrl).then(function(response){
 			        		$scope.residents.push({
-			        			name: response.data.name
+			        			name: response.data.name,
+			        			url: response.data.url
 			        		});
 			            });
             		});
-
             	})
             }
 
