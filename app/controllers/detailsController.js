9def9cdf2cdaf5f6ceba9445380ba80136533a1d
Joel Silva <joelcsilva@outlook.com>
2017-08-02 15:56:37 -0300

    Using new service and creating details

--- app/controllers/detailsController.js
+++ app/controllers/detailsController.js
@@ -1,11 +1,40 @@
 (function () {
     "use strict";
     angular.module('swApp')
-        .controller('detailsController', ['$scope','$location', function ($scope,$location) {
+        .controller('detailsController', 
+        	['$scope','$location', 'swHttpService', 'personDetailService', function ($scope,$location, swHttpService, personDetailService) {
             
             $scope.rotas = function(){
                 $location.path('/');
             };
+
+            var teste = personDetailService.getProducts('url');
+            $scope.residents = [];
+
+            $scope.getPersonDetails = function(){
+            	swHttpService.getPerson(teste).then(function(response){            		
+            		getPlanet(response.data.homeworld);
+            		$scope.personDetails = response.data;
+	            });	            
+            }
+
+            var getPlanet = function(planetUrl){
+            	swHttpService.getPlanet(planetUrl).then(function(response){
+            		$scope.personPlanet = response.data.name;
+            		var residents = response.data.residents;            
+
+            		residents.forEach(function(residentsUrl){
+            			swHttpService.getPerson(residentsUrl).then(function(response){            			        		
+			        		$scope.residents.push({
+			        			name: response.data.name
+			        		});
+			            });	    
+            		});
+            		
+            	})
+            }
+
+            $scope.getPersonDetails();
 
         }]);
 })();
