9da11a02bf2731e4e054a3e1a84edc13bba6023f
Joel Silva <joelcsilva@outlook.com>
2017-08-02 16:03:42 -0300

    Changing variable name

--- app/controllers/detailsController.js
+++ app/controllers/detailsController.js
@@ -1,36 +1,36 @@
 (function () {
     "use strict";
     angular.module('swApp')
-        .controller('detailsController', 
+        .controller('detailsController',
         	['$scope','$location', 'swHttpService', 'personDetailService', function ($scope,$location, swHttpService, personDetailService) {
-            
+
             $scope.rotas = function(){
                 $location.path('/');
             };
 
-            var teste = personDetailService.getProducts('url');
+            var personUrl = personDetailService.getProducts('url');
             $scope.residents = [];
 
             $scope.getPersonDetails = function(){
-            	swHttpService.getPerson(teste).then(function(response){            		
+            	swHttpService.getPerson(personUrl).then(function(response){
             		getPlanet(response.data.homeworld);
             		$scope.personDetails = response.data;
-	            });	            
+	            });
             }
 
             var getPlanet = function(planetUrl){
             	swHttpService.getPlanet(planetUrl).then(function(response){
             		$scope.personPlanet = response.data.name;
-            		var residents = response.data.residents;            
+            		var residents = response.data.residents;
 
             		residents.forEach(function(residentsUrl){
-            			swHttpService.getPerson(residentsUrl).then(function(response){            			        		
+            			swHttpService.getPerson(residentsUrl).then(function(response){
 			        		$scope.residents.push({
 			        			name: response.data.name
 			        		});
-			            });	    
+			            });
             		});
-            		
+
             	})
             }
 
