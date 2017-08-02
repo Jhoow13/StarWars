53b394702689c4511ce4497924055335cf43295c
Joel Silva <joelcsilva@outlook.com>
2017-08-02 02:16:10 -0300

    Creating and using http requests

--- app/controllers/listController.js
+++ app/controllers/listController.js
@@ -9,8 +9,28 @@
             };
 
             swHttpService.getAllPeople().then(function(response){
+            	$scope.peopleList = response.data.results;
+
             	console.log(response.data);
+
+            	$scope.peopleList.forEach(function(person){
+            		getPlanet(person.homeworld);
+
+            		getPersonDetails(person.url);
+            	});
             });
+
+            var getPersonDetails = function(personUrl){
+            	swHttpService.getPerson(personUrl).then(function(response){
+
+	            });
+            }
+
+            var getPlanet = function(planetUrl){
+            	swHttpService.getPlanet(planetUrl).then(function(response){
+
+	            });
+            }
 
         }]);
 })();
--- app/services/swHttpService.js
+++ app/services/swHttpService.js
@@ -2,6 +2,7 @@
     'use strict';
     angular.module('swApp')
         .factory('swHttpService', ['$http', function ($http){
+
             var _getAllPeople = function(){
                 return $http({
                     method: 'GET',
@@ -9,8 +10,24 @@
                 });
             };
 
+            var _getPerson = function(personUrl){
+                return $http({
+                    method: 'GET',
+                    url: personUrl
+                });
+            }
+
+            var _getPlanet = function(planetUrl){
+                return $http({
+                    method:'GET',
+                    url: planetUrl
+                });
+            }
+
             return {
-                getAllPeople: _getAllPeople
+                getAllPeople: _getAllPeople,
+                getPerson: _getPerson,
+                getPlanet: _getPlanet
             };
             
         }]);
